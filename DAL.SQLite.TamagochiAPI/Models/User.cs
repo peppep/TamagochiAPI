d54002db9584edc1a362b336f758d379f2ddcc15
peppep <vlad.krb@gmail.com>
2017-02-03 00:49:55 +0200

    Remove method

--- DAL.SQLite.TamagochiAPI/Models/User.cs
+++ DAL.SQLite.TamagochiAPI/Models/User.cs
@@ -10,14 +10,7 @@ namespace TamagochiAPI.DAL.SQLite.Models
 		public uint UserId { get; set; }
 		public string Name { get; set; }
 		public DateTime LastLogin { get; set; }
-
-		public void Deserialize(SQLiteDataReader reader)
-		{
-			UserId = uint.Parse(reader[0].ToString());
-			Name = reader[1].ToString();
-			LastLogin = new DateTime().FromString(reader[2].ToString());
-		}
-
+		
 		public void Deserialize(DbDataReader reader)
 		{
 			UserId = uint.Parse(reader[0].ToString());
