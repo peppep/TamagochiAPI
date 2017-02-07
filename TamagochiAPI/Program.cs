b54fd95de8db8b9bfcd7cbfcdfdb95714a8517a0
peppep <vlad.krb@gmail.com>
2017-02-08 01:04:40 +0200

    !U add topshelf

--- TamagochiAPI/Program.cs
+++ TamagochiAPI/Program.cs
@@ -1,16 +1,46 @@
 ﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Diagnostics;
+using Topshelf;
 
 namespace TamagochiAPI
 {
+	public class HostingConfiguration : ServiceControl
+	{
+		private IDisposable _webApplication;
+
+		public bool Start(HostControl hostControl)
+		{
+			Trace.WriteLine("Starting the service");
+			_webApplication = WebApp.Start<Startup>("http://localhost:8080");
+			return true;
+		}
+
+		public bool Stop(HostControl hostControl)
+		{
+			_webApplication.Dispose();
+			return true;
+		}
+	}
+
 	public class Program
 	{
-		static void Main(string[] argv)
+		static int Main(string[] argv)
 		{
-			var address = "http://localhost:8080";
-			using (var owin = WebApp.Start<Startup>(address))
+			//var address = "http://localhost:8080";
+			//using (var owin = WebApp.Start<Startup>(address))
+			//{
+			//	System.Console.ReadKey();
+			//}
+			var exitCode = HostFactory.Run(x =>
 			{
-				System.Console.ReadKey();
-			}
+				x.Service<HostingConfiguration>();
+				x.RunAsLocalSystem();
+				x.SetDescription("Owin + Webapi as Windows service");
+				x.SetDisplayName("owin.webapi.test");
+				x.SetServiceName("owin.webapi.test");
+			});
+			return (int)exitCode;
 		}
 	}
 }
